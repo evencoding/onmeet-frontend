967fb8fddfa04c2827dd7ac854723106becef5fd
chloe_choi <plumxeun@gmail.com>
2026-01-11 01:30:03 +0000

    completionId: cgen-177cb7cbfc7a4287a10b0e60513750f8
    cgen-177cb7cbfc7a4287a10b0e60513750f8

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -1,17 +1,32 @@
 import Layout from "@/components/Layout";
 import IntroCard from "@/components/IntroCard";
 import OngoingMeetings from "@/components/OngoingMeetings";
+import CreateMeetingModal from "@/components/CreateMeetingModal";
+import { useState } from "react";
 
 export default function Index() {
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+
+  const handleCreateMeeting = () => {
+    setIsCreateModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsCreateModalOpen(false);
+  };
+
   return (
     <Layout>
       <div className="max-w-4xl space-y-6">
         {/* Intro Card */}
-        <IntroCard />
+        <IntroCard onCreateMeeting={handleCreateMeeting} />
 
         {/* Ongoing Meetings */}
         <OngoingMeetings />
       </div>
+
+      {/* Create Meeting Modal */}
+      <CreateMeetingModal isOpen={isCreateModalOpen} onClose={handleCloseModal} />
     </Layout>
   );
 }
