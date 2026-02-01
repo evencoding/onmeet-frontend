ed5643ad50209a0a1f33915abfec530253956650
chloe_choi <plumxeun@gmail.com>
2026-02-01 04:33:53 +0000

    completionId: cgen-6de839aa13034ff28a8dd1cec750c3fc
    cgen-6de839aa13034ff28a8dd1cec750c3fc

--- client/pages/MeetingRoom.tsx
+++ client/pages/MeetingRoom.tsx
@@ -175,10 +175,19 @@ export default function MeetingRoom() {
         />
 
         {/* Meeting Content */}
-        <div className="flex-1 overflow-hidden flex">
+        <div className="flex-1 overflow-hidden flex relative">
+          {/* Animated background */}
+          <div className="fixed inset-0 -z-10 bg-gradient-to-br from-purple-950 via-black to-purple-900">
+            <div className="absolute inset-0 opacity-40">
+              <div className="absolute top-0 -left-4 w-96 h-96 bg-purple-500 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob"></div>
+              <div className="absolute top-0 -right-4 w-96 h-96 bg-blue-500 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-2000"></div>
+              <div className="absolute -bottom-8 left-20 w-96 h-96 bg-pink-500 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-4000"></div>
+            </div>
+          </div>
+
           {/* Video Area */}
           <div
-            className={`${showParticipants ? "flex-1" : "w-full"} flex flex-col bg-gradient-to-br from-bg-DEFAULT via-white/20 to-bg-DEFAULT`}
+            className={`${showParticipants ? "flex-1" : "w-full"} flex flex-col bg-gradient-to-br from-purple-950/30 via-black/60 to-purple-950/30 relative z-10`}
           >
             {/* Main Video Feed */}
             <div className="flex-1 overflow-hidden p-6 flex flex-col justify-between">
